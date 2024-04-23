38cb1a0efcd0c3e4e508cff50a2379a18a7472e1
SariItani <74187768+SariItani@users.noreply.github.com>
2024-04-23 21:06:28 +0300

    heh

---
+++ sari/python_server/utils.py
@@ -0,0 +1,7 @@
+def process_image(path, flag):
+    if not flag:
+        print("image:", path)
+        return path
+    else:
+        # receive the image from the arduino once the flag is enabled 
+        return # the image
--- sari/python_server/views.py
+++ sari/python_server/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
-from python_server import app
+from python_server import app, utils
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    # call function, get image
+    image = utils.process_image('forset.jpeg')
+    return render_template('index.html', image=image)
